505a7f0a7aae3c2b10ae4a271d4ce3aafb229b27
Derek Allen <derek@getweave.com>
2020-04-04 00:12:13 -0600

    new date

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,14 @@ import './App.css';
 function App() {
   const today = new Date(); 
   const date = { weekday: 'long' };
-  console.log(date);
-  
+  const newDate = today.toLocaleDateString('en-US', date);  
   
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Today is friday!</h1>
+          <h1>Today is {newDate}!</h1>
         </p>
         <a
           className="App-link"
