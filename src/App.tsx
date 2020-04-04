3de23e5df13d1e97b6e2cfe037d51e8bc4f2f7f9
Derek Allen <derek@getweave.com>
2020-04-04 00:17:24 -0600

    removing react stuff

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h1>Today is {newDate}!</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
